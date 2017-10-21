8472792664eb5542dce4358cd749cbd23a727882
Xnco <xuanyylhky@163.com>
2017-10-21 17:59:51 +0800

    添加一些游戏类
    Player(主角)
    GameManager(游戏管理器)
    AppConst(游戏常量)

---
+++ RPGGame/RPGGame/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGGame
+{
+    class GameManager
+    {
+        private static GameManager instance;
+
+        public static GameManager GetSingle()
+        {
+            if (instance == null)
+            {
+                instance = new GameManager();
+            }
+            return instance;
+        }
+
+        private GameManager() { }
+
+        /// <summary>
+        /// 初始化游戏
+        /// </summary>
+        public void InitGame()
+        {
+            // 加载本地文件
+
+        }
+    }
+}
---
+++ RPGGame/RPGGame/Player.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGGame
+{
+    public class Player
+    {
+        public Player(string name)
+        {
+            mName = name;
+        }
+
+        private string mName;
+        private int mLevel;
+        private int mExp;
+        private float mHP;
+        private float mMaxHP;
+        private float mMP;
+        private float mMaxMP;
+
+        public string pName { get { return mName; } }
+
+        public int pLevel
+        {
+            get
+            {
+                return mLevel;
+            }
+            set
+            {
+                mLevel = value;
+            }
+        }
+
+        public int pExp
+        {
+            get
+            {
+                return mExp;
+            }
+
+            set
+            {
+                mExp = value;
+            }
+        }
+
+        public float pHP
+        {
+            get
+            {
+                return mHP;
+            }
+
+            set
+            {
+                mHP = value;
+            }
+        }
+
+        public float pMaxHP
+        {
+            get
+            {
+                return mMaxHP;
+            }
+
+            set
+            {
+                mMaxHP = value;
+            }
+        }
+
+        public float pMP
+        {
+            get
+            {
+                return mMP;
+            }
+
+            set
+            {
+                mMP = value;
+            }
+        }
+
+        public float pMaxMP
+        {
+            get
+            {
+                return mMaxMP;
+            }
+
+            set
+            {
+                mMaxMP = value;
+            }
+        }
+
+    }
+}
